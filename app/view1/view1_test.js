a5e7fe4b7a568df77993d7f2fb46b839555c7f77
小陈 <896530616@qq.com>
2017-06-06 17:26:52 +0800

    Initial commit

---
+++ app/view1/view1_test.js
@@ -0,0 +1,16 @@
+'use strict';
+/* 每个运行的代码下有它对应的单元测试代码，并命名为..._test.js  */
+describe('myApp.view1 module', function() {
+
+  beforeEach(module('myApp.view1'));
+
+  describe('view1 controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var view1Ctrl = $controller('View1Ctrl');
+      expect(view1Ctrl).toBeDefined();
+    }));
+
+  });
+});
